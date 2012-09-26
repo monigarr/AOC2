7b6a877d93545b90d83cfbbcb9d56d92451f885b
Monica Peters <monigarr@yahoo.com>
2012-09-26 17:37:54 -0400

    XCode version 4.5.  Target iPhone iOS 6.  Mountain Lion

--- week1/AOC2_Week1/AOC2_Week1/ViewController.h
+++ week1/AOC2_Week1/AOC2_Week1/ViewController.h
@@ -4,7 +4,9 @@
 //
 //  Created by Monica Peters on 9/26/12.
 //  Copyright (c) 2012 Monica Peters. All rights reserved.
-//
+//  XCode version 4.5
+//  Target iPhone iOS 6
+//  Mountain Lion
 
 #import <UIKit/UIKit.h>
 #import "PetFactory.h"
